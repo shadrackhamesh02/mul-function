cf3e1a5f7165a6bbe97ed78779ca64fde1a44494
shadrack hamesh . S <166796546+shadrackhamesh02@users.noreply.github.com>
2024-05-16 06:49:02 +0530

    Add files via upload

---
+++ multiplication function without default value.py
@@ -0,0 +1,13 @@
+# function without arguments and without return type (default value)
+
+# multipliction function
+
+def mul():
+    a = 10
+    b = 5
+    c = a*b
+    print(c)
+
+# call function 
+
+mul()
